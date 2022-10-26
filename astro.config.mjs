85cdd24c31c58a769656794db641d5b393a147a5
Carson Weeks <mail@carsonweeks.com>
2022-10-25 21:08:21 -0700

    Using edge functions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
