4dc3b589fb2336e49dc50845a8a2cd71aa3e66de
Carson Weeks <mail@carsonweeks.com>
2022-10-25 20:30:31 -0700

    Reinitialized repo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()]
+  integrations: [react(), tailwind(), image()],
+  output: "server",
+  adapter: netlify()
 });
