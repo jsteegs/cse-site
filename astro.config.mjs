3fb186fab236b505e95469dfb29ee7adeaabe55a
Carson Weeks <mail@carsonweeks.com>
2022-10-25 21:09:42 -0700

    Removed netlify for now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
   output: "server",
-  adapter: netlify(),
 });
