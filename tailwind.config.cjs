c5696454f044986c1a5af51003057a4391e21244
Carson Weeks <mail@carsonweeks.com>
2022-10-27 14:15:33 -0700

    Updated colors in tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
         montserrat: ["Montserrat", "system-ui"],
         gillsans: ["Gill Sans", "Georgia"],
       },
+    colors: {
+      'coyote-blue': '#005EB8',
+      'cse-navy': '#101329',
+    }
     },
   },
   plugins: [require("daisyui")],
