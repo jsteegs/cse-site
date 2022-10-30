35ff678fcff0ad9181623095b51d7eead1897207
Carson Weeks <mail@carsonweeks.com>
2022-10-30 14:12:00 -0700

    Added icons for author and pub date.

--- src/components/rss/FeedItem.tsx
+++ src/components/rss/FeedItem.tsx
@@ -6,10 +6,11 @@ import {
   UserCircleIcon,
   UserIcon,
 } from "@heroicons/react/24/solid/esm/index.js";
+import { format } from "date-fns";
 
 const FeedItem = ({ item }: { item: RssItem }) => {
   return (
-    <div className="bg-stone-50 rounded flex mb-6 py-5 px-10 text-slate-700 text-md font-montserrat font-light">
+    <div className="bg-stone-50 rounded  mb-6 py-5 px-10 text-slate-700 text-md font-montserrat font-light">
       <div className="mb-3 flex">
         <div className="mr-5 ">
           <div className="border-2 border-slate-700 rounded-full p-3">
@@ -23,6 +24,16 @@ const FeedItem = ({ item }: { item: RssItem }) => {
         </div>
         <div className=" line-clamp-2 h-fit">{item.title}</div>
       </div>
+      <span>
+        <span className="mr-5">
+          <UserIcon className="h-5 w-5 inline mr-2" />
+          {item.author}
+        </span>
+        <span>
+          <CalendarIcon className="h-5 w-5 inline mr-2" />
+          {format(new Date(item.pubDate), "MM/dd/yyyy")}
+        </span>
+      </span>
     </div>
   );
 };
