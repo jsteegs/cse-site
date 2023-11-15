2ac3e9349970cc9bb624998cd0b808680eff2878
foundationkitty <45774850+foundationkitty@users.noreply.github.com>
2023-11-15 09:13:31 -0800

    add env sample/config var, rev readme, push deps 	new file:   .env.sample 	modified:   README.md 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Website for CSE Club @ CSUSB
 
+Copy [.env.sample](.env.sample) to /.env and make any neccessary edits before running!
+
 Made with Astro: https://github.com/withastro/astro
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
+
 export default defineConfig({
+  site: import.meta.env.VITE_SITE_URL,
   integrations: [
     react(),
     tailwind({
